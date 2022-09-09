114a426bdb6f1f5d006cd5f77f4d433262b9ac49
Tomer Harari <tharari21@gmail.com>
2022-09-09 16:14:31 -0400

    Added styles folder to put styled components

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { NavbarContainer } from "./styles/navbar-styles";
+const Navbar = () => {
+    return (
+        <NavbarContainer>
+            
+        </NavbarContainer>
+    )
+}
+
+export default Navbar;
