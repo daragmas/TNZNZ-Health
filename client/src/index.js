13c6e4bd58e7a286cb1f358ee622798cf303b474
daragmas <daragmas@gmail.com>
2022-09-12 10:54:14 -0400

    gh-pages works, for the most part. Add "TNZNZ-Health" to all your paths

--- client/src/index.js
+++ client/src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Search/>}/>
+      <Route path="TNZNZ-Health/" element={<Search/>}/>
     </Routes>
   </BrowserRouter>
 );
