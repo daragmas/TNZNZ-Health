b85ca55ba9c8b35c3b35691922a6689f30185ad7
Zachary Soliman <zachsoliman1@gmail.com>
2022-09-12 14:34:14 -0400

    styled home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,7 @@
-import { HomeContainer } from "./styles/home-styles";
+import { HomeContainer, Text, Disclaimer, Button } from "./styles/home-styles";
 import Navbar from "./Navbar";
 import ekg from './video/ekg.mp4';
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
@@ -16,13 +17,19 @@ const Home = () => {
             </video>
 
             <div>
-                <h2>What We Do:</h2>
-                <h3>TNZNZ Health is in the business of providing accurate data to YOU. If you are uninsured or underinsured or just need to have a better understanding of medical billing, we're here to help. Below,you can search based on your providers suggestions to find the best pricing for similar procedures at the hospitals in your area.</h3>
-                <p>Disclaimer: if you are experiencing a medical emergency, dial 911 or seek help immediately</p>
-                <p>Disclaimer: no information provided here is guaranteed, this service exists to provide estimates of medical costs and assist in the effort at reducing avoidable medical debt</p>
-                <h1>DO IT FOR GEORGE</h1>
+                <Text>What We Do:</Text>
+                <Text>TNZNZ Health is in the business of providing accurate data to YOU. If you are uninsured or underinsured or just need to have a better understanding of medical billing, we're here to help. Below,you can search based on your providers suggestions to find the best pricing for similar procedures at the hospitals in your area.</Text>
+
+                <Disclaimer>Disclaimer: if you are experiencing a medical emergency, dial 911 or seek help immediately</Disclaimer>
+
+                <Disclaimer>Disclaimer: no information provided here is guaranteed, this service exists to provide estimates of medical costs and assist in the effort at reducing avoidable medical debt</Disclaimer>
+                <Text>*DO IT FOR GEORGE*</Text>
             </div>
-        </HomeContainer>
+            <Link
+                className='Link' to='/search'>
+                <Button>Search by Location</Button>
+            </Link>
+        </HomeContainer >
     )
 }
 
--- client/src/components/styles/home-styles.js
+++ client/src/components/styles/home-styles.js
@@ -1,11 +1,30 @@
 import styled from 'styled-components'
 export const HomeContainer = styled.div`
-    // background-color: black;
+      background-color: rgb(233, 218, 193);
+
 `
 
 export const Text = styled.h1`
-    color: white;
-    font-size:1.2em;
-   
+    color: black;
+    font-size:1.5em;
+    z-index:2;
+`
 
+export const Disclaimer = styled.p`
+    color: red;
+    font-size:1em;
+`
+
+export const Button = styled.button`
+    width: 50vw;
+    height: 5vh;
+    background-color: #54BAB9;
+    border-radius: 5px;
+    border: none;
+    cursor:pointer;
+    :hover {
+        background-color: #9ED2C6;
+        /* color: #9ED2C6; */
+    }
+    
 `
