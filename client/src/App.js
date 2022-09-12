8b19c2a4cba4fa578fd304c105d8815b0890c86b
Zachary Soliman <zachsoliman1@gmail.com>
2022-09-12 14:40:25 -0400

    fixed conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import {useState} from 'react'
+import { useState } from 'react'
 import './App.css';
 import Sidebar from './components/Sidebar'
 import Navbar from './components/Navbar'
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const toggleSidebar = () => {setIsSidebarOpen(prev => !prev)}
+  const toggleSidebar = () => { setIsSidebarOpen(prev => !prev) }
   return (
     <div className="App">
-      <Sidebar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen}/>
+      <Sidebar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
       <Navbar toggleSidebar={toggleSidebar} />
       <Routes>
         {/* Add routes here */}
+
       </Routes>
     </div>
   );
